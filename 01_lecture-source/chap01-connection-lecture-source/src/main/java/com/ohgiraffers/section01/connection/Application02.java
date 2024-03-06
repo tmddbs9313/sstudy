df39defee7bb208f64cbbde27c4ad063d75bd766
choseungyoun <tmddbs9313@naver.com>
2024-03-06 17:52:05 +0900

    3.6

---
+++ 01_lecture-source/chap01-connection-lecture-source/src/main/java/com/ohgiraffers/section01/connection/Application02.java
@@ -0,0 +1,43 @@
+package com.ohgiraffers.section01.connection;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Application02 {
+
+    public static void main(String[] args) {
+
+        String driver = "com.mysql.cj.jdbc.Driver";
+        String url = "jdbc:mysql://localhost/employee";
+        String user = "ohgiraffers";
+        String password = "ohgiraffers";
+
+        Connection con = null;
+
+        try {
+            Class.forName(driver);
+
+            try {
+                con = DriverManager.getConnection(url, user, password);
+
+                System.out.println("con = " + con);
+
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }finally {
+            if(con != null){
+                try {
+                    con.close();
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+
+    }
+}
